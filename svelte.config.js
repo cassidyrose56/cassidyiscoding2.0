3f2228306b1eea99591c2d318fe8d419778aef5f
Cassidy Johnson <crjohnson@legacyscape.com>
2024-04-02 18:00:12 -0600

    updated svelte.config to use vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,11 @@
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
-
   kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '200.html',
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	}
 }
 
