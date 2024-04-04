7cb38262205751f69aff2ec6bdfdac196df8b390
Cassidy Johnson <crjohnson@legacyscape.com>
2024-04-03 19:26:21 -0600

    added target blank to links

--- src/lib/tiles/linkedIn.svelte
+++ src/lib/tiles/linkedIn.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div id="sm-tile" class="relative w-full group md:col-span-2 col-start-2 lg:row-start-1 md:col-start-9 md:row-span-2">
-    <a href="https://www.linkedin.com/in/cassidy-r-johnson/">
+    <a href="https://www.linkedin.com/in/cassidy-r-johnson/" target="_blank">
         <div class="absolute inset-0 bg-tertiary-700 dark:bg-myWhite rounded-lg blur-sm opacity-75 group-hover:bg-opacity-100 group-hover:blur-md group-hover:duration-200 transition duration-1000"></div>
         <div class="relative flex gap-2 w-full bg-tertiary-700 dark:bg-myWhite justify-around items-center h-full md:shadow-md rounded-lg md:p-4">
             <Icon icon="bxl:linkedin-square" class="text-myWhite dark:text-tertiary-700 text-6xl" />
--- src/lib/tiles/medium.svelte
+++ src/lib/tiles/medium.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div id="sm-tile" class="relative group w-full md:col-span-2 col-start-3 lg:row-start-1 md:col-start-11 md:row-span-2">
-    <a href="https://medium.com/@cassidyrose56/">
+    <a href="https://medium.com/@cassidyrose56/" target="_blank">
         <div class="absolute inset-0 bg-myBlack dark:bg-myWhite rounded-lg blur-sm opacity-75 group-hover:bg-opacity-100 group-hover:blur-md group-hover:duration-200 transition duration-1000"></div>
         <div class="relative flex py-4 h-full gap-2 w-full bg-myBlack dark:bg-myWhite justify-around items-center md:shadow-md rounded-lg md:p-4">
             <Icon icon="bxl:medium" class="text-myWhite dark:text-myBlack text-6xl" />
